263122be544cb4ed29738cbff35de4937fe59745
kwangyeon cho <rhkdwh91@gmail.com>
2024-10-13 20:39:07 +0900

    [add] header add

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+} from 'react-native';
+
+export default function Header() {
+    return <View>Head</View>
+}
